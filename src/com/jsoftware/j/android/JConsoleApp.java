f7fb7d4be23fe47addda3274a842e5962f38eef7
bill lam <bbill.lam@gmail.com>
2020-01-03 14:03:57 +0800

    IF64

--- src/com/jsoftware/j/android/JConsoleApp.java
+++ src/com/jsoftware/j/android/JConsoleApp.java
@@ -144,9 +144,11 @@ public class JConsoleApp extends Application
       mVersionCode = -1;
     }
     if (Build.VERSION.SDK_INT < 21) {
-      IF64 = Build.CPU_ABI.contains("64");
+//      IF64 = Build.CPU_ABI.contains("64");
+        IF64 = false;  // always false
     } else {
-      IF64 = Build.SUPPORTED_ABIS[0].contains("64");
+//      IF64 = Build.SUPPORTED_ABIS[0].contains("64");
+      IF64 = android.os.Process.is64Bit();
     }
 //    jversion = getResources().getString(R.string.jversion);
   }
