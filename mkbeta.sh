ec6b96381ac1e9bbf7928af58f9f704028ede4ca
bill lam <bbill.lam@gmail.com>
2022-04-11 11:43:47 +0800

    j904 beta

--- mkbeta.sh
+++ mkbeta.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git grep -l 'com\.jsoftware\.j\.android'|xargs sed -i 's/com\.jsoftware\.j\.android/com.jsoftware.j.beta.android/g'
-sed -i 's/drawable\/jblue/drawable\/jgray/g' AndroidManifest.xml
-sed -i 's/J Android/J Android beta/g' res/values/strings.xml
-sed -i 's/"903"/"904"/g' src/com/jsoftware/j/android/JConsoleApp.java
+git grep -l 'com\.jsoftware\.j\.android'|xargs sed -i "" -e 's/com\.jsoftware\.j\.android/com.jsoftware.j.beta.android/g'
+sed -i "" -e 's/drawable\/jblue/drawable\/jgray/g' AndroidManifest.xml
+sed -i "" -e 's/J Android/J Android beta/g' res/values/strings.xml
+sed -i "" -e 's/"903"/"904"/g' src/com/jsoftware/j/android/JConsoleApp.java
