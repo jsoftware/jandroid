fe3cf7d839d8eaf0f24c675a7ae25651b26890f9
bill lam <bbill.lam@gmail.com>
2023-12-24 10:44:40 +0800

    9.5 -> 9.6

--- mkbeta.sh
+++ mkbeta.sh
@@ -3,4 +3,4 @@ git grep -l 'com\.jsoftware\.j\.android'|xargs sed -i "" -e 's/com\.jsoftware\.j
 sed -i "" -e 's/com\.jsoftware\.j\.android/com.jsoftware.j.beta.android/g' build.gradle
 sed -i "" -e 's/drawable\/jblue/drawable\/jgray/g' AndroidManifest.xml
 sed -i "" -e 's/J Android/J Android beta/g' res/values/strings.xml
-sed -i "" -e 's/"9.4"/"9.5"/g' src/com/jsoftware/j/android/JConsoleApp.java
+sed -i "" -e 's/"9.5"/"9.6"/g' src/com/jsoftware/j/android/JConsoleApp.java
