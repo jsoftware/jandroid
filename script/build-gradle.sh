c92bb53401c6fc67cc1cfe95cdf97c7eb90890da
bill lam <bbill.lam@gmail.com>
2026-04-17 18:50:27 +0800

    gradle

--- script/build-gradle.sh
+++ script/build-gradle.sh
@@ -16,6 +16,9 @@ rm -rf assets/libexec
 cp -r libs assets/libexec
 find assets/libexec -type f -name '*.so' -delete
 find assets/libexec -type f -name 'libj.a' -delete
+find assets/libexec -type f -name 'jamalgam' -delete
+find assets/libexec -type f -name 'hostdefs' -delete
+find assets/libexec -type f -name 'netdefs' -delete
 sed -i -e 's/targetSdkVersion="29"/targetSdkVersion="33"/' AndroidManifest.xml
 sed -i -e 's/android-29/android-33/' project.properties
 ./gradlew build
